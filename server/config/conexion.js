3697dca0db02e867a9d61cb881ae4089969f1454
Ruhlian <159819377+Ruhlian@users.noreply.github.com>
2024-11-28 00:07:04 -0500

    Update conexion.js

--- server/config/conexion.js
+++ server/config/conexion.js
@@ -4,7 +4,8 @@ require('dotenv').config(); // Asegúrate de que dotenv se cargue aquí
 
 // Configuración de conexión a MySQL
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT.
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
